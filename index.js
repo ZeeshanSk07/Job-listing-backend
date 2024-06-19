2314bc620cd6a1ebf032308a48536187a4a29552
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-06-20 00:05:18 +0530

    added database config and initialzation

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const port = 4000;
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch(err => 
+    console.log('Error connecting to database')
+  );
+
+app.get('/health', (req, res) => {
+  res.json({
+    message: 'Health API is working fine',
+    status: 200,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
