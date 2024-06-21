80793dc4f551661a109917e6b83939571fd8b2d6
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-06-21 23:16:50 +0530

    removed verify token from job route

--- model/user.js
+++ model/user.js
@@ -11,6 +11,10 @@ const UserSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    mobile: {
+        type: Number,
+        required: true,
+    },
     password: {
         type: String,
         required: true,
