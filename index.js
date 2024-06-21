9ac5f93f3ca8df99767c4ee4f97921153579e7e0
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-06-21 22:38:08 +0530

    added error handler

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const dotenv = require('dotenv');
 const UserRoute = require('./routes/UserRoute');
 const jobRoute = require('./routes/jobRoute');
 const verifyToken = require('./middleware/verifyToken');
+const errorHandler = require('./middleware/errorHandler');
 
 dotenv.config();
 
-const port = 4000;
+const port = 3000;
 const app = express();
 
 app.use(express.json());
+
+
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Database connected');
@@ -22,11 +25,22 @@ app.use('/user', UserRoute);
 app.use('/job',verifyToken, jobRoute);
 
 app.get('/health', (req, res) => {
+  // res.send
   res.json({
-    message: 'Health API is working fine',
-    status: 200,
+      message: 'Job listing API is working fine',
+      status: 'Working',
+      date: new Date().toLocaleDateString()
   });
 });
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+      message: 'Endpoint not found',
+      status: 'Error',
+  });
+});
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
